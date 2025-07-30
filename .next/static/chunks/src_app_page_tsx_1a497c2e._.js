aa1dcbd077a1e7d02d746a7740d9488af50b30ad
ShamsYoo <shamsalaa118@gmail.com>
2025-07-30 13:02:46 -0500

    Add .next/static/chunks/src_app_page_tsx_1a497c2e._.js 1753898567133

---
+++ .next/static/chunks/src_app_page_tsx_1a497c2e._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_95e94b65._.js",
+  "static/chunks/src_eb18d216._.js"
+],
+    source: "dynamic"
+});
